8e0b4060e37f1c281cbc2b552cb4b7f2695bc2e7
Joao victor <jvrs_souza@hotmail.com>
2023-02-25 21:22:54 -0300

    initial commit

---
+++ grama/grama.ino
@@ -0,0 +1,275 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "Base64.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "PubSubClient.h"
+#include <ArduinoJson.h>
+
+
+const char* ssid = "ALVARENGA";
+const char* password = "46souza46";
+const int ledPin = 4;
+
+const char* mqtt_server = "129.148.56.242";
+const int mqtt_port = 1883;
+const char* mqtt_user = "root";
+const char* mqtt_password = "projetograma";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+  #define PWDN_GPIO_NUM    -1
+  #define RESET_GPIO_NUM   -1
+  #define XCLK_GPIO_NUM    21
+  #define SIOD_GPIO_NUM    26
+  #define SIOC_GPIO_NUM    27
+  
+  #define Y9_GPIO_NUM      35
+  #define Y8_GPIO_NUM      34
+  #define Y7_GPIO_NUM      39
+  #define Y6_GPIO_NUM      36
+  #define Y5_GPIO_NUM      19
+  #define Y4_GPIO_NUM      18
+  #define Y3_GPIO_NUM       5
+  #define Y2_GPIO_NUM       4
+  #define VSYNC_GPIO_NUM   25
+  #define HREF_GPIO_NUM    23
+  #define PCLK_GPIO_NUM    22
+
+#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       32
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_M5STACK_WITHOUT_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       17
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+#else
+  #error "Camera model not selected"
+#endif
+
+
+void takePicture(){
+  camera_fb_t * fb = NULL;
+  digitalWrite(ledPin, HIGH);
+  fb = esp_camera_fb_get();
+  digitalWrite(ledPin, LOW);
+  
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+
+  Serial.printf("Photo information: width = %u, height = %u, format = %u, size = %u\n", fb->width, fb->height, fb->format, fb->len);
+
+//  String imageFile = "data:image/jpeg;base64,";
+  String imageFile = "";
+  char *input = (char *)fb->buf;
+  char output[base64_enc_len(3)];
+  for (int i=0;i<fb->len;i++) {
+    base64_encode(output, (input++), 3);
+    if (i%3==0) imageFile += urlencode(String(output));
+  }
+  esp_camera_fb_return(fb);
+  publish(imageFile.c_str());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Conectando ao servidor MQTT...");
+    if (client.connect("ESP32_Client", mqtt_user, mqtt_password)) {
+      Serial.println("Conectado");
+    } else {
+      Serial.print("Falha, código de erro = ");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+}
+
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
+}
+
+void setup() {
+ 
+  Serial.begin(115200);
+  Serial.setDebugOutput(false);
+  pinMode(ledPin, OUTPUT);
+
+  setupWifi();
+  setupCamera();
+  client.setServer(mqtt_server, mqtt_port);
+
+  while (true){
+    delay(10000);
+    if (WiFi.status() == WL_CONNECTED){
+      if (!client.connected()) {
+        reconnect();
+      }
+      takePicture();
+    }
+  }
+  
+}
+
+void publish(const char* buffer){
+  Serial.println(buffer);  
+  String json = "{\"data\": \"";
+  json += buffer;
+  json += "\"}";
+
+  Serial.println(json.c_str());
+  
+  client.publish("imagem", json.c_str());
+  Serial.println("imagem enviada para o mqtt");
+}
+
+void setupWifi(){
+  // Wi-Fi connection
+  WiFi.begin(ssid, password);
+  Serial.print("Connection");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+}
+
+void setupCamera(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+ if(psramFound()){
+    config.frame_size = FRAMESIZE_QVGA ;
+    config.jpeg_quality = 12;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_QVGA ;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+
+
+void loop() {
+
+}
+
